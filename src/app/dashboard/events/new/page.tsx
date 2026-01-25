bc5e87c9452a4d2ce95906d8672a8bcbd71fe7b7
Pedro Araujo <pedrodearaujo.192@gmail.com>
2026-01-25 17:37:32 +0000

    seleção de data nao ta funcionando (_for element <MonthGrid>_)

--- src/app/dashboard/events/new/page.tsx
+++ src/app/dashboard/events/new/page.tsx
@@ -233,7 +233,7 @@ export default function NewEventPage() {
                                     </FormControl>
                                     </PopoverTrigger>
                                     <PopoverContent className="w-auto p-0" align="start">
-                                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date()} initialFocus />
+                                    <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date(new Date().setHours(0, 0, 0, 0))} initialFocus />
                                     </PopoverContent>
                                 </Popover>
                                 <FormMessage />
